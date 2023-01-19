d43f0869041ba06f5f81eb35b8afcf10f50ad35c
Sofilus <sofia00olsson@gmail.com>
2023-01-19 14:36:03 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+gsap.to("#tå_1", {duration: 2, fill: 'red', delay: 1});
+gsap.to("#tå_2", {duration: 2, fill: 'red', delay: 2});
+gsap.to("#tå_3", {duration: 2, fill: 'red', delay: 3});
+gsap.to("#tå_4", {duration: 2, fill: 'red', delay: 4});
+gsap.to("#trampdyna", {duration: 2, fill: 'red', delay: 5});
+gsap.to("#logga", {duration: 2, rotation: 360, ease: "bounce", transformOrigin: 'center center', delay: 6});
